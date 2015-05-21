8ba43eafb063243ff76675ac1c1e070947f61bd8
Yurii Yarych <yura.yaruch@gmail.com>
2015-05-21 20:31:54 +0200

    Home page styling.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,10 +15,12 @@
 //= require turbolinks
 //= require_tree .
 $(document).ready(function() {
-    $('.main-nav').hide();
-    $('.home-slider').hide();
-    $('.main-nav').fadeIn(2000);
-    $(".home-slider").fadeIn(700, function() {
-        $(".btn-logo").fadeIn(700);
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100){
+            $(".main-nav").css("opacity", "0.9");
+        }
+        else if ($(this).scrollTop() < 100){
+            $(".main-nav").css("opacity", "1.0");
+        }
     });
 });
--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,2 +1,16 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+    $('.main-nav').hide();
+    $('.home-slider').hide();
+    $(".logot").hide();
+    $('.main-nav').fadeIn(2000);
+    $(".logot").fadeIn(3000);
+    $(".home-slider").fadeIn(700);
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100){
+            $(".main-nav").css("opacity", "0.9");
+        }
+        else if ($(this).scrollTop() < 100){
+            $(".main-nav").css("opacity", "1.0");
+        }
+    });
+});
